49393c6efba2c584ea3a74fd58946d0b2e9ecfdd
ShuShuicu <Tomori@miomoe.cn>
2025-02-03 09:28:35 +0800

    修复读取Config.ini bug

--- main.py
+++ main.py
@@ -3,42 +3,31 @@ import os
 import configparser
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-
-# 构建 Assets 的路径
 Assets = os.path.join(current_dir, 'Assets')
-
-# 确保 Config.ini 文件存在并初始化
-config_path = os.path.join(Assets, 'Config.ini')
-if not os.path.exists(config_path):
-    with open(config_path, 'w') as configfile:
-        configfile.write('[Settings]\ndirectory = \n')
 
 class API:
     def __init__(self):
         self.config = configparser.ConfigParser()
-        self.config.read(config_path)
+        self.config.read('Assets/Config.ini')
         self.directory = self.config.get('Settings', 'directory', fallback='')
 
     def select_directory(self):
         # 使用 pywebview 的文件选择对话框
         directory = webview.windows[0].create_file_dialog(webview.FOLDER_DIALOG)
         if directory:
-            self.directory = directory[0]  # create_file_dialog 返回的是一个列表
+            self.directory = directory[0]
             self.config.set('Settings', 'directory', self.directory)
-            with open(config_path, 'w') as configfile:
+            with open('Assets/Config.ini', 'w') as configfile:
                 self.config.write(configfile)
-            return self.directory
-        else:
-            raise Exception("未选择目录")
+        return self.directory
 
     def OpenGames(self):
-        if not self.directory:
-            raise Exception("请先选择游戏目录")
         program_path = os.path.join(self.directory, "LaunchGTAIV.exe")
         if not os.path.exists(program_path):
             return "未找到LaunchGTAIV.exe程序，请检查目录是否正确。"
         os.startfile(program_path)
         return "Ciallo～(∠・ω< )⌒☆"
+
 
 api = API()
 
